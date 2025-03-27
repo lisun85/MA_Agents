55c27b87c48c0f21860deea93c04a4a7637b8d1e
lisun85 <lisun08@gmail.com>
2025-03-26 21:10:33 -0500

    trial with reasoning and s3

--- backend/reasoning_agent/prompts.py
+++ backend/reasoning_agent/prompts.py
@@ -1,5 +1,6 @@
 PROMPT = """
 You are an expert in analyzing companies and matching a specific set of criteria to determine if a company fits that profile. Given the information below of a company determine if the user's criteria such the sector, check size and geographical location matches this company. The top criteria's are sector and check size. The output should be yes or no.
+Please give your best answer. 
 
 Criteria
 Sector: {sector}
